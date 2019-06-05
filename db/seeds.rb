0ba07c15a8ef5306360785208d2de2bbb9e20b87
adam somora <adamsomora33@gmail.com>
2019-06-05 22:10:22 +0200

    Yelp MVP exercise finished fully functional and also green in rake, still needs css touch though

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+my_array = %w[chinese italian japanese french belgian]
+
+puts 'Generating 10 random restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: my_array[rand(my_array.length)]
+  )
+  restaurant.save!
+end
+puts 'Done!'
